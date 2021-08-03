c12112bde82f14574c05ca5d50b7f666e4ec52ae
Colby-Dennis <ncolbywt@gmail.com>
2021-08-03 08:42:44 -0500

    Added simple aiming dot.

--- BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
 // Compiler: 14.29.30038
-#include "D:/GitHub/BattleTank/BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+#include "C:/Users/Colby/OneDrive/Documents/GitHub/BattleTank/BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp.old
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30038
+#include "D:/GitHub/BattleTank/BattleTank/Intermediate/Build/Win64/BattleTankEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
